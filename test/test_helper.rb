3dc41cae6b80d38c0899f91479d80027d32ffb06
Nate Berkopec <nate.berkopec@gmail.com>
2014-12-06 16:31:27 -0500

    STFU Postgres (in test)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+Rails.logger.level = 4
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
