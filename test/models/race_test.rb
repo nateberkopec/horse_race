b44a2ee2062b4c82463a09db1a8185e1ab6fd74d
Nate Berkopec <nate.berkopec@gmail.com>
2014-12-06 16:40:55 -0500

    Refactor race test

--- test/models/race_test.rb
+++ test/models/race_test.rb
@@ -1,23 +1,25 @@
 require 'test_helper'
 
 class RaceTest < ActiveSupport::TestCase
-  test 'setting up a new race' do
-    @race = Race.new(horses: 4.times.map{|i| Horse.new(name: "Horse #{i}")})
-    @race.length = 100
 
+  def setup
+    @race = Race.new(
+      horses: 4.times.map{|i| Horse.new(name: "Horse #{i}", position: 0)},
+      length: 100
+    )
+  end
+
+  test 'setting up a new race' do
     assert @race.save
-    assert_equal 4, @race.horses.size
     assert @race.code
   end
 
   test 'running a race' do
-    @race = Race.new(horses: 4.times.map{|i| Horse.new(name: "Horse #{i}", position: 0)})
-    @race.length = 100
-
     our_horse = @race.horses.first
     100.times { our_horse.position += 1 }
 
     assert @race.finished?
     assert_equal our_horse, @race.winner
   end
+
 end
