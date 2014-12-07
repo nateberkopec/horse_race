fc2fad0d2c9e90e7717427c677acf7cfa125c74a
Nate Berkopec <nate.berkopec@gmail.com>
2014-12-07 17:39:47 -0500

    No shortcuts allowed!

--- app/models/horse.rb
+++ app/models/horse.rb
@@ -3,8 +3,17 @@ class Horse < ActiveRecord::Base
   attr_accessor :race_code
 
   validates :name, presence: true
+  validate :no_shortcuts, if: proc { |h| h.position_changed? }
 
   def race_code=(code)
     self.race = Race.find_or_initialize_by code: code
+  end
+
+  private
+
+  def no_shortcuts
+    unless (-1..1) === (position - position_was)
+      errors.add(:position, 'cannot be increased by more than 1')
+    end
   end
 end
--- test/models/horse_test.rb
+++ test/models/horse_test.rb
@@ -3,9 +3,16 @@ require 'test_helper'
 class HorseTest < ActiveSupport::TestCase
 
   test 'can be added to a race via race_code' do
-    horse = Horse.new(name: 'Seabiscuit', race_code: 'XKRF')
+    @horse = Horse.new(name: 'Seabiscuit', race_code: 'XKRF')
+    assert_equal 'XKRF', @horse.race.code
+  end
 
-    assert_equal 'XKRF', horse.race.code
+  test 'horse position cannot be updated by more than 1' do
+    @horse = horses(:regular_horse)
+    @horse.position += 1
+    assert @horse.valid?
+    @horse.position += 1
+    refute @horse.valid?
   end
 
 end
